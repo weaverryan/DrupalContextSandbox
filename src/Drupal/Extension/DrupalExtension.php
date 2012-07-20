84af1d552deaff86ced9cd430dd7dc2c7373a51c
Ryan Weaver <ryan@thatsquality.com>
2012-07-20 16:50:05 -0500

    Making the RegionSelector work:
      1) Inject the CSSSelector into our RegionSelector since we just want to re-use its CSS awesomeness
      2) Added to our DrupalExtension the ability to take in some "regions" configuration, which is specified
         in behat.yml and tells us what CSS selectors are for each region.
    
    This is now a functioning example!

--- src/Drupal/Extension/DrupalExtension.php
+++ src/Drupal/Extension/DrupalExtension.php
@@ -6,6 +6,7 @@ use Behat\Behat\Extension\Extension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 
 class DrupalExtension extends Extension
 {
@@ -13,7 +14,26 @@ class DrupalExtension extends Extension
   {
     $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/config'));
     $loader->load('services.yml');
+
+    $container->setParameter('drupal.region_map', $config['regions']);
   }
+
+  public function getConfig(ArrayNodeDefinition $builder)
+  {
+    // defining what type of configuration can be passed into this extension
+    $builder->
+      children()->
+        // this says, have a "regions" key, which takes a key-value array
+        arrayNode('regions')->
+          useAttributeAsKey('key')->
+          prototype('variable')->end()->
+        end()->
+      end()
+    ;
+
+  }
+
+
 }
 
 return new DrupalExtension();
--- src/Drupal/Extension/Selector/RegionSelector.php
+++ src/Drupal/Extension/Selector/RegionSelector.php
@@ -3,20 +3,35 @@
 namespace Drupal\Extension\Selector;
 
 use Behat\Mink\Selector\SelectorInterface;
+use Behat\Mink\Selector\CssSelector;
 
+/**
+ * Custom "region" selector to help select Drupal regions
+ */
 class RegionSelector implements SelectorInterface
 {
+  private $cssSelector;
+
+  private $regionMap;
+
+  public function __construct(CssSelector $cssSelector, array $regionMap) {
+    $this->cssSelector = $cssSelector;
+    $this->regionMap = $regionMap;
+  }
+
   /**
    * Translates provided locator into XPath.
    *
-   * @param string $locator current selector locator
-   *
+   * @param string $region
    * @return string
+   * @throws \InvalidArgumentException
    */
-  public function translateToXPath($locator)
-  {
-    var_dump($locator);
-    // todo!
-  }
+  public function translateToXPath($region) {
+    if (!isset($this->regionMap[$region])) {
+      throw new \InvalidArgumentException(sprintf('The "%s" region isn\'t configured!', $region));
+    }
+    $css = $this->regionMap[$region];
 
+    return $this->cssSelector->translateToXPath($css);
+  }
 }
