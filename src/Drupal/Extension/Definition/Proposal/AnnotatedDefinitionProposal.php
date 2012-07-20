7715db13ecdea29659dacd14d18490b313b1ec1b
Ryan Weaver <ryan@thatsquality.com>
2012-07-20 17:07:01 -0500

    Overriding the core AnnotatedDefinitionProposal class to change the definition coding standards
    This depends on the following pull request: https://github.com/Behat/Behat/pull/177
    
    All core classes have their class names defined as a parameter. If you can find the
    right parameter, then you can override the core class (assuming you don't need to change
    the service in any other way).

---
+++ src/Drupal/Extension/Definition/Proposal/AnnotatedDefinitionProposal.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Drupal\Extension\Definition\Proposal;
+
+use Behat\Behat\Definition\Proposal\AnnotatedDefinitionProposal as BaseAnnotatedDefinitionProposal;
+
+class AnnotatedDefinitionProposal extends BaseAnnotatedDefinitionProposal
+{
+  protected function generateSnippet($regex, $methodName, array $args)
+  {
+    return sprintf(<<<PHP
+  /**
+   * @%s /^%s$/
+   */
+  public function %s(%s) {
+      throw new PendingException();
+  }
+PHP
+      , '%s', $regex, $methodName, implode(', ', $args)
+    );
+  }
+
+}
